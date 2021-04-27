19f04a8e595ca61be2fec464f112e4e5cc425c92
Maxi Prez <maxi.prez@gmail.com>
2021-04-27 18:14:14 -0300

    update json

--- src/services/PostService.js
+++ src/services/PostService.js
@@ -1,5 +1,5 @@
 
-const URL_PRODUCTOS = ('https://maxiprez.github.io/cafe-24-7-prez/src/components/data/data.JSON');
+const URL_PRODUCTOS = ('https://maxiprez.github.io/cafe-24-7-prez/src/components/data/dataDetail.JSON');
 
 
 function getItems (){
@@ -12,9 +12,9 @@ function getItems (){
     });
 }
 
-function getItemsDetails (){
+function getItemsDetails (postId){
     return new Promise ((resolve, reject)=>{
-        fetch(`https://maxiprez.github.io/cafe-24-7-prez/src/components/data/dataDetail.JSON`)
+        fetch(`https://my-json-server.typicode.com/maxiprez/cafe-24-7-prez/products${postId}`)
         .then(res => res.json())
         .then(data => resolve(data))
         .catch(err => reject(err))
